32ba422082e5463780f6d49c3a6f4c72b53106fe
GitHub <github-actions@github.com>
2025-01-26 12:05:50 +0000

    Update formula to version v0.0.10

--- quick-capacities.rb
+++ quick-capacities.rb
@@ -1,11 +1,11 @@
 class QuickCapacities < Formula
   desc "CLI for quickly adding information to Capacities using its API"
   homepage "https://abundantsalmon.com"
-  url "https://github.com/AbundantSalmon/QuickCapacities/releases/download/v0.0.9/qc.tar.gz"
-  sha256 "60fca5bc6a3fb6f6c397aa99dfe8e8c5392be52e345819062caf84bbb8db1d33"
+  url "https://github.com/AbundantSalmon/QuickCapacities/releases/download/v0.0.10/qc.tar.gz"
+  sha256 "bf8e0e97e690c890acc164e5686979875576f3ad67765ce80c4b9b6e0a9527ae"
 
   livecheck do
-    url "https://github.com/AbundantSalmon/QuickCapacities/releases/download/v0.0.9/qc.tar.gz"
+    url "https://github.com/AbundantSalmon/QuickCapacities/releases/download/v0.0.10/qc.tar.gz"
     strategy :github_latest
   end
 
